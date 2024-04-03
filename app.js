a6b67991847b64790b709eb7cff7d5714dcdae7d
marakib991 <rarefigure@gmail.com>
2024-04-03 06:22:46 -0400

    dynamic entries are added

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
-const apiKey="3f93dd0f352ae50afbb5b0ccdf527d5c";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=toronto";
+const apiKey= "3f93dd0f352ae50afbb5b0ccdf527d5c";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-const checkWeather = async ()=>{
-  const response= await fetch(apiUrl + `&appid=${apiKey}`);
+const searchBox = document.querySelector('.search input');
+const searchBtn = document.querySelector('.search button');
+const weatherIcon = document.querySelector('.weather-icon');
+
+const checkWeather = async (city)=>{
+  const response= await fetch(apiUrl + city + `&appid=${apiKey}`);
   var data = await response.json();
 
   console.log(data);
 
   document.querySelector('.city').innerHTML = data.name;
-  document.querySelector('.temp').innerHTML = data.main.temp;
-  document.querySelector('.humidity').innerHTML = data.main.humidity;
-  document.querySelector('.wind').innerHTML = data.wind.speed;
-
+  document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + "°c";
+  document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
+  document.querySelector('.wind').innerHTML = data.wind.speed + " km/h";
+  
+  if(data.weather[0].main == 'Clouds'){
+    weatherIcon.src = "images/clouds.png";
+  }
+  else if(data.weather[0].main == 'Clear'){
+    weatherIcon.src = "images/clear.png";
+  }
+  else if(data.weather[0].main == 'Rain'){
+    weatherIcon.src = "images/rain.png";
+  }
+  else if(data.weather[0].main == 'Drizzle'){
+    weatherIcon.src = "images/drizzle.png";
+  }
+  else if(data.weather[0].main == 'Mist'){
+    weatherIcon.src = "images/mist.png";
+  }
 }
 
-checkWeather();
+searchBtn.addEventListener('click',()=>{
+  checkWeather(searchBox.value);
+})
+
+
 
